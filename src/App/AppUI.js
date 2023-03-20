ba9171fa181ed72d32d8dc479f5aff08a6aa86b6
Alfred024 <21030761tcelaya.edu.mx>
2023-03-20 16:45:19 -0600

    Modal para usePortal hook creado e implementado

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -5,6 +5,7 @@ import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from "../Modal";
 
 function AppUI() {
 
@@ -22,24 +23,28 @@ function AppUI() {
             <TodoCounter/>
             <TodoSearch/>
 
-        {/* El provider está en el archivo de la Carpeta del TodoContext*/}
-        <TodoList>
-            {/* Usamos el operador && como una forma de expresar un "entonces..." (es una mamada) */}
-            {/* {error && <p>Hay un error...</p>} */}
-            {loading && <p>Cargando...</p>}
-            {(!loading && !searchedTodos.length) && <p>No hay todos, crea uno</p>}
-            {searchedTodos.map(todo => (
-            <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-            />
-            ))}
-        </TodoList>
-
-            <CreateTodoButton />
+          {/* El provider está en el archivo de la Carpeta del TodoContext*/}
+          <TodoList>
+              {/* Usamos el operador && como una forma de expresar un "entonces..." (es una mamada) */}
+              {/* {error && <p>Hay un error...</p>} */}
+              {loading && <p>Cargando...</p>}
+              {(!loading && !searchedTodos.length) && <p>No hay todos, crea uno</p>}
+              {searchedTodos.map(todo => (
+              <TodoItem
+                  key={todo.text}
+                  text={todo.text}
+                  completed={todo.completed}
+                  onComplete={() => completeTodo(todo.text)}
+                  onDelete={() => deleteTodo(todo.text)}
+              />
+              ))}
+          </TodoList>
+          <Modal>
+            <p>
+              Aquí ira el modal
+            </p>
+          </Modal>
+          <CreateTodoButton />
         </React.Fragment>
     );
 }
